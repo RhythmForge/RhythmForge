41042959c8d084024feb6f2982d98c53bcd8710e
Snabbis <44020985+TheSnabbis@users.noreply.github.com>
2025-01-24 22:32:18 +0000

    Feature: gateway now relays messages irc -> osu

--- app/irc.py
+++ app/irc.py
@@ -1,6 +1,5 @@
 import irc.client
 import irc.bot
-import irc.client_aio
 
 import asyncio
 import threading
@@ -8,57 +7,67 @@ import threading
 from app.logging import Ansi
 from app.logging import log
 
-from typing import TYPE_CHECKING
+import app.state.sessions as session
 
-import app.state
-from app.repositories import channels as channel_repo
-from app.objects.player import Player
-if TYPE_CHECKING:
-    from app.objects.channel import Channel
+loop = 0
+irc_gateway = 0
 
 class IrcGateway(irc.client.SimpleIRCClient):
-    def __init__(self, target):
+    def __init__(self):
         irc.client.SimpleIRCClient.__init__(self)
-        self.target = target
+        self.targets = session.channels
     
     def on_welcome(self, c, e):
-        log("IRC: Joining channel", Ansi.LGREEN)
-        if irc.client.is_channel(self.target):
-            c.join(self.target)
+        for channel in self.targets:
+            if irc.client.is_channel(channel.name):
+                log(f"IRC: Joining channel {channel.name}", Ansi.LGREEN)
+                c.join(channel.name)
     
     def on_pubmsg(self, c, e):
-        log("IRC: Message rcvd", Ansi.LGREEN)
-        self.transmit_message(e.arguments[0], 'snabbis', '#osu')
-        # tomorrow: use ensure_future to call async transmit_message and pull name from db
+        log(f"IRC: {e.source.nick}: {e.arguments[0]}", Ansi.LGREEN)
+        global loop
+        self.future = asyncio.ensure_future(
+            self.transmit_message_to_osu(e.arguments[0], e.source.nick, e.target), loop=loop
+        )
 
-    def transmit_message(self, message, sender, target):
-        target_channel = app.state.sessions.channels.get_by_name(target)
-        player = app.state.sessions.players.get(name=sender)
-        if player is not None:
-            target_channel.send(message, player)
-            if player.is_online:
-                player.send(message, player, target_channel)
+    async def transmit_message_to_osu(self, message, sender, target):
+        target_channel = session.channels.get_by_name(target)
+        if target_channel is None:
+            return
+        player = await session.players.from_cache_or_sql(name=sender)
+        if player is None:
+            return
+        
+        target_channel.send(message, player)
+        if player.is_online and not player.is_bot_client:
+            player.send(message, player, target_channel)
 
-def irc_thread():
-    irc_client = IrcGateway('#osu')
-    
+    def transmit_message_to_irc(self, message, sender, target):
+        # TODO: help??? ;_;
+        #self.connection.privmsg(target.name, message)
+        pass
+
+def irc_thread(irc_gateway : IrcGateway):
     try:
-        irc_client.connect(server='192.168.1.31', port=6667, nickname='testBOT')
+        irc_gateway.connect(server='192.168.1.31', port=6667, nickname='testBOT')
     except irc.client.ServerConnectionError as x:
         log(x, Ansi.RED)
     
     log("IRC: BOT started", Ansi.LGREEN)
     
     try:
-        irc_client.start()
+        irc_gateway.start()
     finally:
-        irc_client.connection.disconnect()
+        irc_gateway.connection.disconnect()
 
 def start_irc_handler():
     log("IRC: Starting BOT", Ansi.LGREEN)
+    global irc_gateway
+    irc_gateway = IrcGateway()
     global loop
     loop = asyncio.get_event_loop()
-    thread = threading.Thread(target=irc_thread)
+    
+    thread = threading.Thread(target=irc_thread, args=(irc_gateway,))
     thread.daemon = True
     thread.start()
     
